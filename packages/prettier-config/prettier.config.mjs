5a2410afe88deeeb3b4db376711a9a35dfb8246a
devwqc <devwqc@gmail.com>
2025-05-06 11:50:20 +0900

    feat(@repo/prettier-config): prettier-config 패키지 추가

---
+++ packages/prettier-config/prettier.config.mjs
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
+
+export default config;

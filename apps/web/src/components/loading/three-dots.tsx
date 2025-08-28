14bcefbb57dac15874beef613a6aba6fd9a929b8
devwqc <devwqc@gmail.com>
2025-08-28 09:27:01 +0900

    feat(web): createAnimation 기반의 ThreeDots 구현

--- apps/web/src/components/loading/three-dots.tsx
+++ apps/web/src/components/loading/three-dots.tsx
@@ -1,28 +1,35 @@
-import clsx from 'clsx';
+'use client';
+
+import createAnimation from '@/helpers/animation';
+import { CSSProperties } from 'react';
+
+const animationName = createAnimation(
+  'bc-three-dots',
+  '0%, 100% { opacity: 1; } 50% { opacity: 0.5; }',
+);
+
+const getStyle = (delay: number): CSSProperties => {
+  return {
+    height: '60px',
+    width: '60px',
+    backgroundColor: '#3497DB',
+    borderRadius: '50%',
+    animation: `${animationName} 2s cubic-bezier(0.4, 0, 0.6, 1) infinite`,
+    animationDelay: `${delay}s`,
+  };
+};
 
 const ThreeDots = () => (
-  <div className={clsx('w-fit', 'flex items-center gap-2')}>
-    <div
-      className={clsx(
-        'h-[60px] w-[60px]',
-        'bg-[#3497DB]',
-        'animate-pulse rounded-full [animation-delay:0s] [animation-duration:1.5s]',
-      )}
-    ></div>
-    <div
-      className={clsx(
-        'h-[60px] w-[60px]',
-        'bg-[#3497DB]',
-        'animate-pulse rounded-full [animation-delay:0.5s] [animation-duration:1.5s]',
-      )}
-    ></div>
-    <div
-      className={clsx(
-        'h-[60px] w-[60px]',
-        'bg-[#3497DB]',
-        'animate-pulse rounded-full [animation-delay:1s] [animation-duration:1.5s]',
-      )}
-    ></div>
+  <div
+    style={{
+      display: 'inline-flex',
+      alignItems: 'center',
+      gap: '8px',
+    }}
+  >
+    <div style={getStyle(0)} />
+    <div style={getStyle(0.5)} />
+    <div style={getStyle(1)} />
   </div>
 );
 
